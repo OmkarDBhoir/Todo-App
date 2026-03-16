c77620189a713a784915b82e57977405914b9a0a
Omkar <omkar123b@gmail.com>
2026-03-16 21:53:18 +0530

    inital backend added

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import type {Request, Response} from "express";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Server is running");
+})
+
+
+app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
+
+
